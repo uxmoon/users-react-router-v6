f8190c528e991cfde3adeb799383c90fe5844c0e
Martin Luna <maluna@technisys.com>
2022-01-14 15:51:06 -0300

    Added 404 example

--- readme.md
+++ readme.md
@@ -10,3 +10,11 @@ Based on [React Router v6 - Guía práctica desde CERO | Tutorial](https://youtu
 - Nest multiple `Route` components inside `Routes`
 - `Routes` replaces `Switch`
 - `element` replaces `component` and `render` props
+
+### 404 not found
+
+- Use `*` for `path` prop to setup a 404 page when there's no path match
+- The order of the routes won't affect other routes if the 404 route is declared first
+- `Navigate` replaces `Redirect`
+- Import `Navigate` and use it inside element, set the `path`
+- Add the `replace` prop if you don't want to keep the navigation history
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -8,6 +8,8 @@ ReactDOM.render(
       <Route path="/" element={<h1>Home</h1>} />
       <Route path="/users" element={<h1>Users</h1>} />
       <Route path="/about" element={<h1>About</h1>} />
+      {/* <Route path="*" element={<h1>404 page not found</h1>} /> */}
+      <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
