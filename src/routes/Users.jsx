ff4b2261623e58bb443ebd4b846de7d848da4375
Martin Luna <maluna@technisys.com>
2022-01-14 16:55:29 -0300

    Map over user list and use id to set Link

--- src/routes/Users.jsx
+++ src/routes/Users.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+import { getAllUsers } from "../users";
+
 export default function Users() {
+  const users = getAllUsers();
   return (
     <div>
       <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={user.id.toString()}>{user.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
