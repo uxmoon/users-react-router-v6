f3879518f91cde0fbfa3326261f68ae690867609
Martin Luna <maluna@technisys.com>
2022-01-14 15:40:02 -0300

    Added example routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/users" element={<h1>Users</h1>} />
+      <Route path="/about" element={<h1>About</h1>} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
