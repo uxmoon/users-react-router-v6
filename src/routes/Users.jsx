262b6c5fdae8bc8970f9bde29f789a4260189d43
Martin Luna <maluna@technisys.com>
2022-01-14 18:37:22 -0300

    Added searchParams hook

--- src/routes/Users.jsx
+++ src/routes/Users.jsx
@@ -1,11 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { getAllUsers } from "../users";
 
 export default function Users() {
   const users = getAllUsers();
+  const [searchParam, setSearchParams] = useSearchParams();
+  const filter = searchParam.get("filter") ?? "";
+  const handleFilter = (event) => {
+    setSearchParams({ filter: event.target.value });
+  };
   return (
     <div>
       <h1>Users</h1>
+      <input
+        value={filter}
+        onChange={handleFilter}
+        type="text"
+        placeholder="Filter users"
+      />
       <ul>
         {users.map((user) => (
           <li key={user.id}>
