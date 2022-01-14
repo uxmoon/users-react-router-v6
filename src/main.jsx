8126fad71f12f31bae8a2122dd17341daddcc53e
Martin Luna <maluna@technisys.com>
2022-01-14 16:54:34 -0300

    Added User to display single user details

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./routes/Home";
 import Users from "./routes/Users";
+import User from "./routes/User";
 import About from "./routes/About";
 import Layout from "./Layout";
 
@@ -12,6 +13,7 @@ ReactDOM.render(
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="users" element={<Users />} />
+        <Route path="users/:userId" element={<User />} />
         <Route path="about" element={<About />} />
         {/* <Route path="*" element={<h1>404 page not found</h1>} /> */}
         <Route path="*" element={<Navigate replace to="/" />} />
